bf314a11dd870a5cfdfae51d4e4804d0abebad63
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-05-24 19:21:42 +0200

    Loop inspector interface

--- src/main/java/com/demo/codewars/get_the_loop/Node.java
+++ src/main/java/com/demo/codewars/get_the_loop/Node.java
@@ -1,28 +1,5 @@
 package com.demo.codewars.get_the_loop;
 
-import java.util.Objects;
-
-public class Node {
-    private final Node node;
-
-    public Node(Node node) {
-        this.node = node;
-    }
-
-    public Node getNext() {
-        return node;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node node1 = (Node) o;
-        return Objects.equals(node, node1.node);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(node);
-    }
+public interface Node {
+    Node getNext();
 }

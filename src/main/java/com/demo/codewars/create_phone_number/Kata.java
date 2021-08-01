458c777e7de82560def850946cee92560b96ca37
Dominik Kondrot <dominik.kondrot@gmail.com>
2021-08-01 23:27:38 +0200

    Create phone number implementation https://www.codewars.com/kata/525f50e3b73515a6db000b83/

---
+++ src/main/java/com/demo/codewars/create_phone_number/Kata.java
@@ -0,0 +1,22 @@
+package com.demo.codewars.create_phone_number;
+
+/*
+Description:
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the
+form of a phone number.
+
+Example:
+Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+ */
+
+public class Kata {
+
+    public static String createPhoneNumber(int[] numbers) {
+        String phoneNumber = "(xxx) xxx-xxxx";
+        for (int i : numbers) phoneNumber = phoneNumber.replaceFirst("x", Integer.toString(i));
+        return phoneNumber;
+    }
+
+}
